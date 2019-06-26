69c3cc9c0ce56e79a88ab8620cbb9422939b1ce2
Carlos Eduardo Avellaneda Suárez <cavellaneda@belatrix.com>
2019-06-26 11:56:50 -0500

    Added comments

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -6,12 +6,17 @@ files = glob.glob('pdfs/*.pdf')
 
 def pdf_to_df(pdf):
     df = read_pdf(pdf)
+    # Remove NAs columns:
     df.dropna(axis=1, how='all', inplace=True)
+    # Rename columns:
     df.columns = ['measure', 'ref', 'pre', 'pre%', 'ci', 'post', 'post%', 'post_chg']
+    # Drop wrong rows:
     df.dropna(axis=0, subset=['measure'], inplace=True)
+    # Get IDs from filename:
     df['id'] = re.sub(r'(pdfs[/\\]+)(\d+)(_.*)', r'\2', pdf)
-    df2 = df.set_index(['id', 'measure']).unstack('measure')
-    return df2
+    # Reshape data:
+    df = df.set_index(['id', 'measure']).unstack('measure')
+    return df
 
 dfs = []
 for file in files:
